d1bdc30a4e483a475997ef2a6e6128d705ab86d2
ashishrawat7 <ashish895352@gmail.com>
2020-02-27 16:40:29 +0530

    update

--- first-app/src/Todo.js
+++ first-app/src/Todo.js
@@ -1,47 +1,31 @@
-import React from 'react';
-import  data1 from './data.json';
+import React from "react";
+import data1 from "./data.json";
 
-
-class Todo extends React.Component{
-    //  getData = (prop)=>{
-    //      return data1.map((val, key)=>{
-    //          return <li key={key}>
-    //              <input
-    //               checked={val.checked}
-    //               type="checked" 
-    //               />
-    //              {val.name}
-    //              </li>
-    //     })
-    //  }
-    handleChange= ()=>{
-        
-    }
-render(){
-    return(
-        <div className="todoApp">
-            <h3>Todo App</h3>
-            <ul>
-            {
-          data1.map((val, key)=>{
-              return <li key={key}>
-                  <input
+class Todo extends React.Component {
+  handleChange = () => {};
+  render() {
+    return (
+      <div className="todoApp">
+        <h3>Todo App</h3>
+        <ul>
+          {data1.map((val, key) => {
+            return (
+              <li key={key}>
+                <input
                   checked={val.checked}
-                    type="checkbox" 
-                    onChange={this.handleChange}
-                   />
-                   
-                  <span>{val.name}</span>
-                  {
-                     val.checked ? <span className="badge badge-secondary ">Checked</span>  : null
-                  }
-                  </li>
-         })
-      }
-            </ul>
-           
-        </div>
-    )
-}
+                  type="checkbox"
+                  onChange={this.handleChange}
+                />
+                <span>{val.name}</span>
+                {val.checked ? (
+                  <span className="badge badge-secondary ">Checked</span>
+                ) : null}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 export default Todo;
