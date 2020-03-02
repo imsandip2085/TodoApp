ffccffd1e1a67599350f8db24212306edbbf9fd5
ashishrawat7 <ashish895352@gmail.com>
2020-03-02 11:25:33 +0530

    complete-todoApp

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,18 +1,48 @@
-import React from 'react';
-import './css/app.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Todo from './Todo';
-import Forms from './Form';
+import React from "react";
+import "./css/app.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Todo from "./Todo";
+import Forms from "./Form";
+import data1 from "./data.json";
 
-class App extends React.Component{
+// import { Item } from 'react-bootstrap/lib/Breadcrumb';
 
-  render(){
-    return(
-    <div >
-       <Todo />
-       <Forms />
-    </div>
-    )
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Items: "",
+      data: data1
+    };
+  }
+  addNewItem = Item => {
+    let newDataArray = this.state.data;
+    if(Item !== "" ){
+    newDataArray.push({ id: 1, name: Item, checked: false });
+    }
+    this.setState({ data: newDataArray });
+  };
+  handleChangeEvent = Items => {
+    console.log(Items);
+    let data = this.state.data.map(val => {
+      if (val.name === Items) {
+        val.checked = !val.checked;
+      }
+      return val
+    });
+  
+    this.setState({ data: data });
+  };
+  render() {
+    return (
+      <div>
+        <Todo
+          data={this.state.data}
+          handleChangeEvent={this.handleChangeEvent}
+        />
+        <Forms addNewItem={this.addNewItem} />
+      </div>
+    );
   }
 }
 export default App;
--- first-app/src/Form.js
+++ first-app/src/Form.js
@@ -1,15 +1,47 @@
-import React from 'react';
+import React from "react";
 
-
-class Forms extends React.Component{
-    render(){
-        return(
-            <div className="todoApp">
-                <h4>Todo</h4>
-                <input type="text" className="input"></input>
-                <button type="submit">submit</button>
-            </div>
-        )
+class Forms extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      onclick: true
+    };
+  }
+  
+  handleSubmit = e => {
+    this.props.addNewItem(this.state.title)
+    this.setState({ title: "" });
+    e.preventDefault();
+    if(this.state.title === ""){
+      this.setState({ onclick : false})
+    }else{
+      this.setState({onclick : true})
     }
+  };
+
+  handleChange = event => {
+    this.setState({ title: event.target.value });
+  };
+  render() {
+    return (
+      <div className="todoApp">
+        <h4>Todo</h4>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            className={this.state.onclick ? "input1":"input2"}
+            placeholder="Todo..."
+            value={this.state.title}
+            onChange={this.handleChange}
+          ></input>
+          <br />
+          <button type="submit" className="buttonForm">
+            submit
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 export default Forms;
